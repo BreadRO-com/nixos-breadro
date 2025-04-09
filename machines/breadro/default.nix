f3c09ec9329683bbf666a1b8ce3c35cde8559408
MakiseKurisu <MakiseKurisu@users.noreply.github.com>
2025-04-09 21:48:25 +0800

    feat: add download server

--- machines/breadro/default.nix
+++ machines/breadro/default.nix
@@ -52,12 +52,75 @@
         };
         http = host: host // {
           rejectSSL = true;
-        }; in {
-        "_" = https { locations = {
-          "/".return = "404";
-        };};
+        };
+      in {
+        "_" = https {
+          locations = {
+            "/" = {
+              return = "404";
+            };
+          };
+        };
+        "dl.breadro.com" = https {
+          locations = {
+            "/" = {
+              root = "/srv/dl";
+              basicAuth = {
+                breadro = "toastro";
+              };
+              extraConfig = ''
+                autoindex on;
+              '';
+            };
+          };
         };
       };
+    };
+    
+    samba-wsdd = {
+      enable = true;
+      openFirewall = true;
+      interface = "10.1.0.1";
+    };
+    samba = {
+      enable = true;
+      openFirewall = true;
+      settings = {
+        global = {
+          "invalid users" = [
+            "root"
+          ];
+          "passwd program" = "/run/wrappers/bin/passwd %u";
+          security = "user";
+          workgroup = "WORKGROUP";
+          "server string" = "breadro";
+          "netbios name" = "breadro";
+          # "use sendfile" = "yes";
+          # "max protocol" = "smb2";
+          # note: localhost is the ipv6 localhost ::1
+          "hosts allow" = [
+            "10.1.0."
+            "127.0.0.1"
+            "localhost"
+          ];
+          "hosts deny" = [
+            "0.0.0.0/0"
+          ];
+          "guest account" = "nobody";
+          "map to guest" = "bad user";
+        };
+        dl = {
+          path = "/srv/dl";
+          browseable = "yes";
+          "read only" = "no";
+          "guest ok" = "no";
+          "create mask" = "0644";
+          "directory mask" = "0755";
+          "force group" = "users";
+          "acl allow execute always" = "yes";
+        };
+      };
+    };
   };
 
   users.users = {
@@ -65,6 +128,7 @@
       isNormalUser = true;
       extraGroups = [
         "wheel"
+        "nginx"
       ];
       openssh.authorizedKeys.keys = [
         "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDNbARwJPNYn4MUPHvy7JRR7B+Yh2t50K7xUbMvdf58U1IPYOfDB818atx0MoJvvMro7H3NXateXMnFW6h111FkeTN4e6pLePIOCIyX20S9U6rq85T81ePTi9ied6SP6IEpyGEdWO73eiXbZAOj9VPnXOir3tvrKRNISz3mHp163NT7HMHRJjZ+9xCUhqPzw0VrKD3fTbrljdKk8Rfpd0wDvv2Nb6DA+nfvYME3w1ICU73Y4oP2x+Sx6epqr/FXk6vBsrKdyxPEALirCtct8LYYrt1KxTI2yfodr9kiOFgPIMwzuKPRixV2S15Eh5NwL5Hi6+RNQRXu82V8osSFUC0OypFplmTrY5yAHzDQB5DOYWlRG4KeKACd/tB2HMuW46qWIxngXYR2WSoAHFDdSuKj+fTsb21uQ+LvoQU6mnfUyYDokHuDPMi4iUlgFpcmyeNq1Dm7OD0LWLRbIdpJYgtd4aT9uT3XIQ8Ic8X/sZuNTv1jGLDhZMdV/awHtfDggtE= excalibur@yuntian"
@@ -74,6 +138,7 @@
       isNormalUser = true;
       extraGroups = [
         "wheel"
+        "nginx"
       ];
     };
   };
